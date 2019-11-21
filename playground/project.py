fc31636f4a910943cec3da1e915fc1e66761eb9d
Dandan Meng <mengdandanno1@163.com>
2019-11-21 09:01:50 +0800

    Lesson 3-6: add price and description

--- playground/project.py
+++ playground/project.py
@@ -19,6 +19,11 @@ def HelloWorld():
     for i in items:
         output += i.name
         output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output += '</br>'
+        output += '</br>'
     return output
 
 if __name__ == '__main__':
