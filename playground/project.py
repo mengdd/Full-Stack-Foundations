3a6b768fbca5c4ccbd446c858123986a91502c37
Dandan Meng <mengdandanno1@163.com>
2019-11-27 08:48:09 +0800

    Lesson 3-16: message flashing for edit and delete

--- playground/project.py
+++ playground/project.py
@@ -41,6 +41,7 @@ def editMenuItem(restaurant_id, menu_id):
             editedItem.name = request.form['name']
         session.add(editedItem)
         session.commit()
+        flash("Menu Item has been edited")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template(
@@ -54,6 +55,7 @@ def deleteMenuItem(restaurant_id, menu_id):
     if request.method == 'POST':
         session.delete(itemToDelete)
         session.commit()
+        flash("Menu Item has been deleted")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('deletemenuitem.html', item=itemToDelete)
